c105dce3cc3a4f8212ee82fb979f1b30ffcd574e
Andrew <seongchan24@gmail.com>
2021-10-12 12:13:51 +0900

    임성찬: 로그인 구현 완료

--- app/controller/signIn.js
+++ app/controller/signIn.js
@@ -17,17 +17,20 @@ exports.signInFn = async (req, res) => {
     //회원정보 암호화
     const token = Jwt.sign({ userId: isUser.userId }, 'mini-project');
     console.log(userId);
-    return res.status(201).send({
-      token: token,
-      userId: userId,
-      message: '로그인 성공!',
-    });
+    return res.status(201)
+              .send({
+                result: 'success',
+                token: token,
+                userId: userId,
+                successMsg: '로그인 성공!',
+              });
   } else {
     console.log('유저확인 실패');
     console.log('로그인 실패');
     return res.status(200).send({
       // 로그인에 실패했으니 다시 로그인 창을 보낼까요? window.location.herf='/login'
       // status 200인 이유는 콘솔창의 오류문구를 보기 싫은..마음..
+      result: 'failure',
       errorMsg: '로그인 실패!',
     });
   }
