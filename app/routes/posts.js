41554d450f56bd27af06152027a094586a4e2452
jjangjae <nick2866@naver.com>
2021-10-15 11:45:15 +0900

    장재원 post 부분 my sql 변환

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -7,21 +7,18 @@ const authMW = require("../middlewares/authMW");
 router.post("/posts", ctrlPost.getProcess.getPost); //게시글 조회
 router.post("/posts/write", ctrlPost.postProcess.writePost); //게시글 등록
 router.get("/posts/detail/:postId", ctrlPost.getProcess.detailPost); //게시글 상세 페이지
-router.put("/posts/detail/:postId/edit", authMW, ctrlPost.postProcess.editPost); //게시글 수정
-router.delete(
-  "/posts/detail/:postId/edit",
-  authMW,
-  ctrlPost.postProcess.deletePost
-); //게시글 삭제
-router.post(
-  "/posts/detail/:postId/like",
-  authMW,
-  ctrlLike.likeProcess.createLike
-); //좋아요 추가
-router.delete(
-  "/posts/detail/:postId/like",
-  authMW,
-  ctrlLike.likeProcess.removeLike
-); //좋아요 삭제
+router.put("/posts/detail/:postId/edit", ctrlPost.postProcess.editPost); //게시글 수정
+//게시글 삭제
+router.delete("/posts/detail/:postId/edit", ctrlPost.postProcess.deletePost);
+// router.post(
+//   "/posts/detail/:postId/like",
+//   authMW,
+//   ctrlLike.likeProcess.createLike
+// ); //좋아요 추가
+// router.delete(
+//   "/posts/detail/:postId/like",
+//   authMW,
+//   ctrlLike.likeProcess.removeLike
+// ); //좋아요 삭제
 
 module.exports = router;
