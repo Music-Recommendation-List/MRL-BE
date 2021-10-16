0de453ef5967e8c25745f7cdc965b26c8b8254c6
jjangjae <88707474+JangJaeWon22@users.noreply.github.com>
2021-10-16 12:01:13 +0900

    불필요 폴더 삭제

--- MRL-TOTAL/controller/users/ctrl.userAuth.js
+++
@@ -1,7 +0,0 @@
-const userAuthController = async (req, res) => {
-  const { targetUserInfo } = res.locals;
-  console.log(targetUserInfo);
-  res.send({ targetUserInfo });
-};
-
-module.exports = userAuthController;
